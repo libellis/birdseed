c8d7efba7e2a883600d98ebdc53d64f0fc112e2e
Peter Farr <Peter@PrismaPhonic.com>
2019-03-02 22:04:11 -0800

    Finished all crud for users model

--- src/models/user.rs
+++ src/models/user.rs
@@ -21,3 +21,14 @@ pub struct NewUser<'a> {
     pub last_name: &'a str,
     pub is_admin: bool,
 }
+
+// Intentionally leaving out username because it's PK and is_admin because a user shouldn't be able
+// to change that
+#[derive(Deserialize, AsChangeset, Default, Clone)]
+#[table_name = "users"]
+pub struct UpdateUserData {
+    pub password: Option<String>,
+    pub email: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+}
